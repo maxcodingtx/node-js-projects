64fd9d2d40eac25cfda900f870e2001f7fd3e006
maxcodingtx <maxcodingtx@gmail.com>
2025-03-07 20:06:22 -0600

    added scripting to disable input field after change. included username with the message when sent to the server.

--- basic-chat-app/frontend/app.js
+++ basic-chat-app/frontend/app.js
@@ -1,19 +1,31 @@
 const socket = io();
 const chatBox = document.getElementById("chatBox");
+const userNameInput = document.getElementById("userName");
 const messageInput = document.getElementById("userMessage");
 const sendBtn = document.getElementById("sendMessage");
 
 sendBtn.addEventListener("click", () => {
     const message = messageInput.value;
-    if (message.trim()) {
-        socket.emit("chat message", message);
+    const userName = userNameInput.value;
+    if (!userName) {
+        alert("Please enter a username");
+        return;
+    }
+
+    if (userName.trim()) {
+        socket.emit("chat message", `${userName}: ${message}`);
         messageInput.value = "";
     }
 });
 
 socket.on("chat message", msg => {
-    const messageElement = document.createElement("div");
+    const messageElement = document.createElement("p");
     messageElement.textContent = msg;
     chatBox.appendChild(messageElement);
     chatBox.scrollTop = chatBox.scrollHeight;
+});
+
+// lock user input field once the user has entered the username
+userNameInput.addEventListener("change", () => {
+    userNameInput.disabled = true;
 });
