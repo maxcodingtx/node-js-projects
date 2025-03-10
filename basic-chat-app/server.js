ce2cd9abc7be6f957618dff65c25cf731ed7091e
maxcodingtx <maxcodingtx@gmail.com>
2025-03-10 18:25:05 -0500

    Added comments to server code.

--- basic-chat-app/server.js
+++ basic-chat-app/server.js
@@ -1,26 +1,36 @@
+// importing modules
 const express = require("express");
 const { Server } = require("socket.io");
 const http = require("http");
 const PORT = 4000;
 
+// creating express app
 const app = express();
+// creating server
 const server = http.createServer(app);
+// creating socket
 const io = new Server(server);
 
+// linking the frontend to the server.
 app.use(express.static("frontend"));
 
+// creating a socket connection when a user connects.
 io.on("connection", socket => {
+    // when user connect, log it
     console.log("A user connected");
 
+    // when user sends a message, broadcast it.
     socket.on("chat message", msg => {
         io.emit("chat message", msg);
     });
 
+    // when user disconnects, log it.
     socket.on("disconnect", () => {
         console.log("A user disconnected");
     });
 });
 
+// listening to the PORT
 server.listen(PORT, () => {
     console.log(`Server is listening at the PORT: ${PORT}`);
 });
