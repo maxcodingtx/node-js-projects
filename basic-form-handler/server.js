56a4daf4eb4bf413f0ab0761f5e86ada4e569cca
maxcodingtx <maxcodingtx@gmail.com>
2025-03-11 20:32:05 -0500

    added comments

--- basic-form-handler/server.js
+++ basic-form-handler/server.js
@@ -1,12 +1,20 @@
+// importing express
 const express = require("express");
+// creating express app
 const app = express();
+// defining a port number
 const PORT = 3000;
 
+// Middleware to parse URL-encoded data (for form submit)
 app.use(express.urlencoded({ extended: true }));
+// Connecting to frontend "public" folder
 app.use(express.static("public"));
+// Middleware to parse JSON data
 app.use(express.json());
 
+// Handle POST request to "/submit" endpoint
 app.post("/submit", (req, res) => {
+    // Sending JSON response containing form data from frontend
     res.json({
         message: "Data received",
         data: req.body,
@@ -14,6 +22,7 @@ app.post("/submit", (req, res) => {
     });
 });
 
+// Starting the server on specified port.
 app.listen(`${PORT}`, () => {
     console.log(`server on port ${PORT}`);
 });
